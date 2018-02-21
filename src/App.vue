e422126125547b80a8e2647a903ba19ded602675
milan.zivanov <milan.zivanov@gmail.com>
2018-02-21 23:11:24 +0100

    props in the action and add remove

--- src/App.vue
+++ src/App.vue
@@ -4,31 +4,36 @@
       <div class="container-fluid">
           <div class="row align-items-center">
               <div class="col-md-4">
-                  <a class="navbar-brand" href="#">Logo menu</a>
+                  <a class="navbar-brand" href="#">M</a>
               </div>
               <div class="col-md-8">
                   Currency
               </div>
           </div>
-          <!-- <h1>{{msg}}</h1>
-          <div class="d-flex p-2">I'm a flexbox container!</div> -->
       </div>
     </div>
 
 	<div class="main-content">
       <div class="container-fluid h-100">
           <div class="row h-100">
-              <div class="col-md-4 h-100">
-                  <p>something</p>
-                  <ul class="list-group list-group-flush">
-                      <li class="list-group-item">Cras justo odio1</li>
-                      <li class="list-group-item">Cras justo odio2</li>
-                      <li class="list-group-item">Cras justo odio3</li>
+              <div class="col-md-4 bg-info h-100">
+                  <h5 class="">Currency List</h5>
+                  <ul class="list-group list-group-flush list-group-bg">
+                    <li class="list-group-item
+                               d-flex
+                               justify-content-between align-items-center"
+                        v-for="(data, index) in currencies" :key='index'>{{ index + 1 }}. {{data.id}}
+                        <i class="fa fa-minus-circle"    v-on:click="remove(index)">
+                        </i>
+                    </li>
                   </ul>
+                  <a href="#" class="badge badge-primary">Add Currencies</a>
               </div>
               <div class="col-md-8 bg-primary">
-                  magic
+                  <!-- new component -->
+                  <app-add v-bind:currencies="currencies"></app-add>
               </div>
+
           </div>
       </div>
 	</div>
@@ -36,11 +41,43 @@
 </template>
 
 <script>
+    import  Add from './components/Add.vue';
+
 export default {
+
     name: 'app',
+    components: {
+      'app-add': Add
+    },
+
     data () {
         return {
-        msg: 'Welcome to Your Vue.js App'
+        // v-imodel
+        id: '',
+        currencies: [
+            { id: "euro",
+              iso: "eur",
+              simbol: "☼"
+            },
+            { id: "srpski dinar",
+              iso: "rsd",
+              simbol: "☼"
+            },
+            { id: "kuna",
+              iso: "rhk",
+              simbol: "☼"
+            }
+        ]
+    }
+  },
+  methods: {
+    //     addId() {
+    //         this.currencies.push({id: this.id});
+    //         this.id = '';
+    //     }
+
+    remove(index) {
+        this.currencies.splice(index, 1);
     }
   }
 }
@@ -49,11 +86,16 @@ export default {
 <style lang="scss">
     @import '../node_modules/bootstrap/scss/bootstrap.scss';
     @import url('https://fonts.googleapis.com/css?family=Open+Sans');
+    @import url("https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css");
 
 body, html {
 	font-family: 'Open Sans', sans-serif;
     height: 100%;
     background-color: #eee;
+}
+
+*, *:before, *:after {
+  box-sizing: border-box;
 }
 
 .header {
@@ -63,5 +105,22 @@ body, html {
 .main-content {
     height: calc(100vh - 72px);
 }
+
+.list-group-bg {
+    background-color: peru;
+}
+
+.list-group-item {
+    background-color: transparent;
+}
+
+ input {
+    width: calc(100% - 40px);
+    border: 0;
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #323333;
+    color: #687F7F;
+  }
 
 </style>
---
+++ src/components/Add.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="add-component">
+        <!-- v-imodel -->
+        <form @submit.prevent="addId">
+            <input type="text"
+                    placeholder="Enter a currency you want..."
+                    v-model="id">
+        </form>
+
+        <!-- props in the action -->
+        <!-- <p>{{currencies}}</p> -->
+        <!-- <ul>
+            <li v-for="(data, index) in currencies" :key='index'>
+              {{ index + 1 }}. {{data.id}}</li>
+        </ul> -->
+
+    </div>
+</template>
+
+<script>
+
+  import App from './Add.vue';
+
+  export default {
+
+    // props in the action
+    // props: ['currencies'],
+    props: {
+      currencies: {
+        type: Array,
+        required: true
+      }
+    },
+
+    name: 'Add',
+
+    data () {
+      return {
+        id: ''
+      }
+    },
+
+    methods : {
+    addId() {
+        this.currencies.push({id: this.id});
+        this.id = '';
+    }
+  }
+
+  }
+</script>
+
+<style>
+
+</style>
+
+
